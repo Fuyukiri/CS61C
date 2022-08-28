22231dc6ae16b05a9fa45f097f0e7efdd5b6401c
wallcracker <ns34gp@hotmail.com>
2022-08-27 19:09:15 -0700

    finish all lab1

--- fa20-lab-starter/lab01/ll_cycle.c
+++ fa20-lab-starter/lab01/ll_cycle.c
@@ -3,5 +3,14 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    node *fast = head;
+    node *slow = head;
+    while (fast != NULL && fast->next != NULL) {
+        fast = fast->next->next;
+        slow = slow->next;
+        if (fast == slow) {
+            return 1;
+        }
+    }
     return 0;
 }
