d342a76309d63376b733bb11aaa2dabc3a3f12d0
Fuyukiri <ns34gp@hotmail.com>
2022-08-29 23:58:00 -0700

    format imageloader.c

--- project/fa20-proj1-starter/imageloader.c
+++ project/fa20-proj1-starter/imageloader.c
@@ -20,29 +20,31 @@
 #include <string.h>
 #include "imageloader.h"
 
-//Opens a .ppm P3 image file, and constructs an Image object. 
-//You may find the function fscanf useful.
-//Make sure that you close the file with fclose before returning.
-Image *readData(char *filename) 
+// Opens a .ppm P3 image file, and constructs an Image object.
+// You may find the function fscanf useful.
+// Make sure that you close the file with fclose before returning.
+Image *readData(char *filename)
 {
-	//YOUR CODE HERE
+	// YOUR CODE HERE
 	char format[10];
 	uint32_t cols, rows, scale;
-	Image* img = (Image *) malloc(sizeof(Image));
+	Image *img = (Image *)malloc(sizeof(Image));
 
-	FILE * fp;
-	fp = fopen (filename, "r");
+	FILE *fp;
+	fp = fopen(filename, "r");
 
 	fscanf(fp, "%s %u %u %u", format, &cols, &rows, &scale);
 
 	img->rows = rows;
 	img->cols = cols;
-	Color** image = (Color**) malloc(rows * sizeof(Color *));
+	Color **image = (Color **)malloc(rows * sizeof(Color *));
 	img->image = image;
 
-	for (int i = 0; i < rows ; i++) {
-		image[i] = (Color *) malloc (cols * sizeof(Color));
-		for (int j = 0; j < cols; j++) {
+	for (int i = 0; i < rows; i++)
+	{
+		image[i] = (Color *)malloc(cols * sizeof(Color));
+		for (int j = 0; j < cols; j++)
+		{
 			fscanf(fp, "%hhu %hhu %hhu", &image[i][j].R, &image[i][j].G, &image[i][j].B);
 		}
 	}
@@ -50,16 +52,18 @@ Image *readData(char *filename)
 	return img;
 }
 
-//Given an image, prints to stdout (e.g. with printf) a .ppm P3 file with the image's data.
+// Given an image, prints to stdout (e.g. with printf) a .ppm P3 file with the image's data.
 void writeData(Image *image)
 {
-	//YOUR CODE HERE
+	// YOUR CODE HERE
 	uint32_t cols = image->cols;
 	uint32_t rows = image->rows;
-	Color** img = image->image;
-	printf("P3\n%u %u\n255\n",cols, rows);
-	for (int i = 0; i < rows ; i++) {
-		for (int j = 0; j < cols - 1; j++) {
+	Color **img = image->image;
+	printf("P3\n%u %u\n255\n", cols, rows);
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols - 1; j++)
+		{
 			printf("%3hhu %3hhu %3hhu   ", img[i][j].R, img[i][j].G, img[i][j].B);
 		}
 		printf("%3hhu %3hhu %3hhu", img[i][cols - 1].R, img[i][cols - 1].G, img[i][cols - 1].B);
@@ -67,13 +71,14 @@ void writeData(Image *image)
 	}
 }
 
-//Frees an image
+// Frees an image
 void freeImage(Image *image)
 {
 	// uint32_t cols = image->cols;
 	uint32_t rows = image->rows;
-	Color** img = image->image;
-	for (int i = 0; i < rows ; i++) {
+	Color **img = image->image;
+	for (int i = 0; i < rows; i++)
+	{
 		free(img[i]);
 	}
 	free(img);
