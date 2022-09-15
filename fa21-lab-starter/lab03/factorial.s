1afeb3d280aeb1b1f058e981b1c5cf709df71a1d
Fuyukiri <ns34gp@hotmail.com>
2022-09-14 22:08:38 -0700

    lab03 exercise 4 simpler version

--- fa21-lab-starter/lab03/factorial.s
+++ fa21-lab-starter/lab03/factorial.s
@@ -22,14 +22,12 @@ main:
 
 factorial:
     # YOUR CODE HERE
-    add t0, x0, a0 # t0 = a0 = 8 (current number)
-    addi s1, x0, 1 # s1 = 1 (product base)
+    addi t0, a0, -1 # t0 = a0 -1 = 7 (current number)
     addi s2, x0, 1 # s2 = 1 (use to minus 1)
 loop:
     beq t0, x0, exit # while t0 != 0
-    mul s1, s1, t0 # a0 = previous product * current number
+    mul a0, a0, t0 # a0 = previous product * current number
     sub t0, t0, s2 # t0 -= 1
     jal x0, loop # loop
 exit:
-    add a0, x0, s1 # a0 = res
     jr ra  # return a0
