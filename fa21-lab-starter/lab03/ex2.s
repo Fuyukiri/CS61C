77559f02c4fb1052b01bcb8d202e84637c0b14a6
Fuyukiri <ns34gp@hotmail.com>
2022-09-14 21:56:46 -0700

    lab03 exercise 3 4

--- fa21-lab-starter/lab03/ex2.s
+++ fa21-lab-starter/lab03/ex2.s
@@ -23,10 +23,10 @@ dest:
 
 .text
 fun:
-    addi t0, a0, 1
-    sub t1, x0, a0
-    mul a0, t0, t1
-    jr ra
+    addi t0, a0, 1  # t0 = a0(x) + 1 = 0x00000003 + 1 = 4
+    sub t1, x0, a0  # t1 = -x = -3
+    mul a0, t0, t1  # a0 = 4 * -3 = -12 (0xFFFFFFF4)
+    jr ra # return
 
 main:
     # BEGIN PROLOGUE
@@ -37,29 +37,29 @@ main:
     sw s3, 12(sp)
     sw ra, 16(sp)
     # END PROLOGUE
-    addi t0, x0, 0
-    addi s0, x0, 0
-    la s1, source
-    la s2, dest
+    addi t0, x0, 0 # t0(k) = 0
+    addi s0, x0, 0 # s0(sum) = 0
+    la s1, source  # s1 -> source[0] address 0x10000000
+    la s2, dest    # s2 -> dest[0] address 0x1000001C
 loop:
-    slli s3, t0, 2
-    add t1, s1, s3
-    lw t2, 0(t1)
-    beq t2, x0, exit
-    add a0, x0, t2
-    addi sp, sp, -8
-    sw t0, 0(sp)
-    sw t2, 4(sp)
-    jal fun
-    lw t0, 0(sp)
-    lw t2, 4(sp)
-    addi sp, sp, 8
-    add t2, x0, a0
-    add t3, s2, s3
-    sw t2, 0(t3)
-    add s0, s0, t2
-    addi t0, t0, 1
-    jal x0, loop
+    slli s3, t0, 2  # s3 = t0(k) << 2 = 0x00000000 || s3 = 1 << 2 = 0x100 = 4 || 2 << 2 = 8
+    add t1, s1, s3  # t1 = s1 + s3 = 0x10000000 -> source[0] address || source[1] (4 bytes for a word)
+    lw t2, 0(t1)    # t2 = 0x00000003 = source[0]
+    beq t2, x0, exit # t2 != 0
+    add a0, x0, t2  # a0 = t2 = 0x00000003 = source[0]
+    addi sp, sp, -8 # sp = 0x7FFFFFC4 (+8)
+    sw t0, 0(sp)  # save t0(k) (0)
+    sw t2, 4(sp)  # save t2 (0x00000003)
+    jal fun       # --> fun() | a0 = 0xFFFFFFF4(-12)
+    lw t0, 0(sp)  # load t0(k) = 0
+    lw t2, 4(sp)  # load t2 = 3
+    addi sp, sp, 8 # sp = 0x7FFFFFCC (+8)
+    add t2, x0, a0 # t2 = a0 = -12
+    add t3, s2, s3 # t3 = 0x1000001C + 0 (dest[0] address)
+    sw t2, 0(t3)  # dest[0] = t2 = -12
+    add s0, s0, t2 # s0 = s0 + t2 = -12
+    addi t0, t0, 1 # t0 + 1 k++
+    jal x0, loop # return to loop start location
 exit:
     add a0, x0, s0
     # BEGIN EPILOGUE
--- fa21-lab-starter/lab03/factorial.s
+++ fa21-lab-starter/lab03/factorial.s
@@ -5,12 +5,12 @@ n: .word 8
 
 .text
 main:
-    la t0, n
-    lw a0, 0(t0)
-    jal ra, factorial
+    la t0, n # t0 n address
+    lw a0, 0(t0) # a0 = t0[0] = 8
+    jal ra, factorial 
 
-    addi a1, a0, 0
-    addi a0, x0, 1
+    addi a1, a0, 0 # a1 = 8
+    addi a0, x0, 1 # a0 = 1
     ecall # Print Result
 
     addi a1, x0, '\n'
@@ -22,3 +22,14 @@ main:
 
 factorial:
     # YOUR CODE HERE
+    add t0, x0, a0 # t0 = a0 = 8 (current number)
+    addi s1, x0, 1 # s1 = 1 (product base)
+    addi s2, x0, 1 # s2 = 1 (use to minus 1)
+loop:
+    beq t0, x0, exit # while t0 != 0
+    mul s1, s1, t0 # a0 = previous product * current number
+    sub t0, t0, s2 # t0 -= 1
+    jal x0, loop # loop
+exit:
+    add a0, x0, s1 # a0 = res
+    jr ra  # return a0
