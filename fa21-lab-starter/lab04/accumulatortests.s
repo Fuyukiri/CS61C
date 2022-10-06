5c55f472e239e4a1d04d057c268f8a961c1f51c0
Fuyukiri <ns34gp@hotmail.com>
2022-10-05 22:00:17 -0700

    finish lab04

--- fa21-lab-starter/lab04/accumulatortests.s
+++ fa21-lab-starter/lab04/accumulatortests.s
@@ -2,22 +2,47 @@
 
 .data
 inputarray: .word 1,2,3,4,5,6,7,0
+inputarray_zero: .word 0,2,3,4,5,6,7,0
 
 TestPassed: .asciiz "Test Passed!"
 TestFailed: .asciiz "Test Failed!"
 
 .text
 # Tests if the given implementation of accumulate is correct.
-#Input: a0 contains a pointer to the version of accumulate in question. See lotsofaccumulators.s for more details
+# Input: a0 contains a pointer to the version of accumulate in question. See lotsofaccumulators.s for more details
 #
 #
 #
 #The main function currently runs a simple test that checks if accumulator works on the given input array. All versions of accumulate should pass this.
 #Modify the test so that you can catch the bugs in four of the five solutions!
 main:
-    la a0 inputarray
-    jal accumulatorone
-    li t0 28
+    # test 1
+    # la a0 inputarray
+    # jal accumulatorone
+    # li t0 28
+    # beq a0 t0 Pass
+
+    # test 2
+    # la a0 inputarray
+    # addi sp, sp, -4
+    # sw t0, 4(sp)
+    # jal accumulatortwo
+    # li t0 28
+    # lw t0, 4(sp)
+    # addi sp, sp, 4
+    # beq a0 t0 Pass
+
+    # test 3
+    # la a0 inputarray
+    # li t2 1
+    # jal accumulatorfour
+    # li t0 28
+    # beq a0 t0 Pass
+
+    # test 4
+    la a0 inputarray_zero
+    jal accumulatorfive
+    li t0 0
     beq a0 t0 Pass
 Fail:
     la a0 TestFailed
--- fa21-lab-starter/lab04/megalistmanips.s
+++ fa21-lab-starter/lab04/megalistmanips.s
@@ -62,24 +62,31 @@ map:
     # - 4 for the size of the array
     # - 4 more for the pointer to the next node
 
-    # also keep in mind that we should not make ANY assumption on which registers
-    # are modified by the callees, even when we know the content inside the functions 
-    # we call. this is to enforce the abstraction barrier of calling convention.
+
 mapLoop:
-    add t1, s0, x0      # load the address of the array of current node into t1
+    lw t1, 0(s0)        # load the address of the array of current node into t1
     lw t2, 4(s0)        # load the size of the node's array into t2
 
-    add t1, t1, t0      # offset the array address by the count
+    slli t3, t0, 2
+    add t1, t1, t3      # offset the array address by the count
     lw a0, 0(t1)        # load the value at that address into a0
 
+    addi sp, sp, -8
+    sw ra, 0(sp)
+    sw t1, 4(sp)
+
     jalr s1             # call the function on that value.
+
+    lw ra, 0(sp)
+    lw t1, 4(sp)
+    addi sp, sp, 8
 
     sw a0, 0(t1)        # store the returned value back into the array
     addi t0, t0, 1      # increment the count
     bne t0, t2, mapLoop # repeat if we haven't reached the array size yet
 
-    la a0, 8(s0)        # load the address of the next node into a0
-    lw a1, 0(s1)        # put the address of the function back into a1 to prepare for the recursion
+    lw a0, 8(s0)        # load the address of the next node into a0
+    mv a1, s1           # put the address of the function back into a1 to prepare for the recursion
 
     jal  map            # recurse
 done:
@@ -87,6 +94,7 @@ done:
     lw s1, 4(sp)
     lw ra, 0(sp)
     addi sp, sp, 12
+    jr ra
 
 print_newline:
     li a1, '\n'
