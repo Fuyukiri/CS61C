925476fbaa8ef6b1adc3faf07323575bbef2f7ea
Fuyukiri <11898971+Fuyukiri@users.noreply.github.com>
2022-11-16 23:58:09 -0800

    proj3 task8 update

--- project/fa21-proj3-starter/auto_test.sh
+++ project/fa21-proj3-starter/auto_test.sh
@@ -4,4 +4,8 @@ python3 tests/part-b/custom/unittest-generator.py
 
 python3 tools/create-test.py tests/part-b/custom/inputs/*.s
 
+# for windows
+# python3 .\tools\create-test.py (get-item .\tests\part-b\custom\inputs\*.s)
+
 python3 test.py tests/part-b/custom/
+# python3 test.py .\tests\part-b\custom\
